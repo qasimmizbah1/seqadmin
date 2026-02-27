ba4c3ee222455af0d881e3f0e51bad5f71c44ba0
qasim <qasimmizbah@gmail.com>
2026-02-27 10:53:19 +0530

    Added Cybersecurity content type

---
+++ src/api/cybersecurity/services/cybersecurity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cybersecurity service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cybersecurity.cybersecurity');
