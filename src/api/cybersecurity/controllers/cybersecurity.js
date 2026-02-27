72f6ec92dda91e0e040c313dcd5ca41a21d208b7
qasim <qasimmizbah@gmail.com>
2026-02-27 11:47:36 +0530

    Remove cybersecurity API

--- src/api/cybersecurity/controllers/cybersecurity.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cybersecurity controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cybersecurity.cybersecurity');
